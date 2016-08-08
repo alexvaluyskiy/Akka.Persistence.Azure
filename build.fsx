27790a7e5d4ee925dc9382f0d22440925ff61ac7
ravengerUA <yurisikorsky@gmail.com>
2016-08-08 22:01:45 +0300

    Fixed nuget

--- build.fsx
+++ build.fsx
@@ -19,7 +19,7 @@ cd __SOURCE_DIRECTORY__
 
 let product = "Akka.NET"
 let authors = [ "Akka.NET Team" ]
-let copyright = "Copyright © 2013-2015 Akka.NET Team"
+let copyright = "Copyright © 2013-2016 Akka.NET Team"
 let company = "Akka.NET Team"
 let description = "Akka.NET is a port of the popular Java/Scala framework Akka to .NET"
 let tags = ["akka";"actors";"actor";"model";"Akka";"concurrency"]
@@ -103,7 +103,7 @@ Target "CopyOutput" <| fun _ ->
         let src = "src" @@ project @@ @"bin/Release/"
         let dst = binDir @@ project
         CopyDir dst src allFiles
-    [ "Akka.Persistence.Azure"
+    [ "Akka.Persistence.AzureTable"
       ]
     |> List.iter copyOutput
 
